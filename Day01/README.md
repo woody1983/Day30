175256f98498aa6804342ea2efffc50e52313a29
root <root@pom-inner-test.asia.phx>
2014-04-23 15:43:19 +0800

    socket

---
+++ Day01/README.md
@@ -0,0 +1,12 @@
+##Day01 Apr23
+
+> 老规矩 先占坑
+
+* Ruby/Socket_Server 看API先看到这个章节 忽然想到一个关于自动化监控Replication的想法
+
+
+
+### Point 1
+现在监控MySQL的Replication是一个问题 如果能做到在一个Server端一个命令就可以知道所有slave状况的话也挺不错的
+
+有了想法 去学就比较好玩一点了 `话说我是多无聊 会去看API` 
---
+++ Day01/socket_server.rb
@@ -0,0 +1,14 @@
+require 'socket'
+
+server = TCPServer.new 1983
+
+loop do
+  client = server.accept
+  puts "Client is on board " if client
+  client.puts "Hello, who are you?"
+  client.puts "Time is #{Time.now}"
+  client.close
+  puts "Someone is disconnect!" unless client
+end
+
+
